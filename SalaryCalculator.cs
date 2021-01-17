88805082e70a579050d9791dec4812f21d5a9356
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-17 15:06:35 +0000

    Add files via upload

---
+++ SalaryCalculator.cs
@@ -0,0 +1,89 @@
+﻿
+class SalaryCalculator
+{
+
+    public int hoursWorked;
+
+    public string employeeName;
+
+    public decimal hourlyPay;
+
+    public int normalHours;
+
+    public int excessHours;
+
+    public decimal overTime;
+
+    double increment = 1.5;
+
+    
+
+
+    public void SetHoursWorked(int salaryCalculatorHoursWorked)
+    {
+        hoursWorked = salaryCalculatorHoursWorked;
+    }
+
+    public int GetHoursWorked()
+    {
+        return hoursWorked;
+    }
+
+    public void SetEmployeeName(string salaryCalculatorEmployeeName)
+    {
+        employeeName = salaryCalculatorEmployeeName;
+    }
+
+    public string GetEmployeeName()
+    {
+        return employeeName;
+    }
+
+    public void SetHourlyPay(decimal salaryCalculatorHourlyPay)
+    {
+        hourlyPay = salaryCalculatorHourlyPay;
+    }
+
+    public decimal GetHourlyPay()
+    {
+        return hourlyPay;
+    }
+
+    public int ExcessHours()
+    {
+        if(hoursWorked > 40)
+        {
+            excessHours = hoursWorked - 40;
+            return excessHours;
+        }
+        else
+        {
+            excessHours = 0;
+            return excessHours;
+        }
+        
+    }
+
+    public decimal OverTime()
+    {
+        overTime = excessHours * (decimal)increment;
+        return overTime;
+    }
+
+    public int NormalHours()
+    {
+        if (hoursWorked > 40)
+        {
+            normalHours = hoursWorked - excessHours;
+            return normalHours;
+        }
+        else
+        {
+            normalHours = hoursWorked;
+            return normalHours;
+        }
+            
+    }
+
+}
+
---
+++ SalaryCalculatorTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class SalaryCalculatorTest
+{
+    static void Main()
+    {
+        SalaryCalculator salaryCalculator1 = new SalaryCalculator();
+        SalaryCalculator salaryCalculator2 = new SalaryCalculator();
+        SalaryCalculator salaryCalculator3 = new SalaryCalculator();
+
+        Console.Write("Please enter the name of the 1st employee: ");
+        string salaryCalculator1theEmployeetheName = Console.ReadLine();
+        salaryCalculator1.SetEmployeeName(salaryCalculator1theEmployeetheName);
+
+        Console.Write($"Please enter the hourly wage of {salaryCalculator1.GetEmployeeName()}: ");
+        decimal salaryCalculator1theHourlyPay = decimal.Parse(Console.ReadLine());
+        salaryCalculator1.SetHourlyPay(salaryCalculator1theHourlyPay);
+
+        Console.Write($"Please enter the hours worked by {salaryCalculator1.GetEmployeeName()}: ");
+        int salaryCalculator1theHoursWorked = int.Parse(Console.ReadLine());
+        salaryCalculator1.SetHoursWorked(salaryCalculator1theHoursWorked);
+
+        Console.Write("\n\nPlease enter the name of the 2nd employee: ");
+        string salaryCalculator2theEmployeetheName = Console.ReadLine();
+        salaryCalculator2.SetEmployeeName(salaryCalculator2theEmployeetheName);
+
+        Console.Write($"Please enter the hourly wage of {salaryCalculator2.GetEmployeeName()}: ");
+        decimal salaryCalculator2theHourlyPay = decimal.Parse(Console.ReadLine());
+        salaryCalculator2.SetHourlyPay(salaryCalculator2theHourlyPay);
+
+        Console.Write($"Please enter the hours worked by {salaryCalculator2.GetEmployeeName()}: ");
+        int salaryCalculator2theHoursWorked = int.Parse(Console.ReadLine());
+        salaryCalculator2.SetHoursWorked(salaryCalculator2theHoursWorked);
+
+        Console.Write("\n\nPlease enter the name of the 3rd employee: ");
+        string salaryCalculator3theEmployeetheName = Console.ReadLine();
+        salaryCalculator3.SetEmployeeName(salaryCalculator3theEmployeetheName);
+
+        Console.Write($"Please enter the hourly wage of {salaryCalculator3.GetEmployeeName()}: ");
+        decimal salaryCalculator3theHourlyPay = decimal.Parse(Console.ReadLine());
+        salaryCalculator3.SetHourlyPay(salaryCalculator3theHourlyPay);
+
+        Console.Write($"Please enter the hours worked by {salaryCalculator3.GetEmployeeName()}: ");
+        int salaryCalculator3theHoursWorked = int.Parse(Console.ReadLine());
+        salaryCalculator3.SetHoursWorked(salaryCalculator3theHoursWorked);
+
+        Console.Write("\n\n");
+
+        PrintSalary(salaryCalculator1);
+        
+        PrintSalary(salaryCalculator2);
+        
+        PrintSalary(salaryCalculator3);
+                
+    }   
+
+    static void PrintSalary(SalaryCalculator all)
+    {
+
+        Console.Write($"{all.employeeName} has worked {all.hoursWorked} hours on the relevant week.");
+        Console.WriteLine($"The hourly pay of {all.employeeName} is {all.hourlyPay}.");
+        Console.WriteLine($"He/she has {all.ExcessHours()} hour(s) overtime.");
+        Console.WriteLine($"The total pay for the week of {all.employeeName} is {(all.OverTime() + all.NormalHours()) * all.hourlyPay}.\n");
+        
+    }
+}
+
